9dc7c682620282442217368d1da6561119e06c4c
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2019-03-23 00:30:53 +0200

    added tasks generation, calling rand seed only once

--- generator/hw1-task1/generator2.py
+++ generator/hw1-task1/generator2.py
@@ -6,17 +6,18 @@ def create_tree(n):
   parent.append(1)
   children = []
   for i in range(1, n):
-      random.seed(len(parent))
       children.append(parent[random.randint(0,len(parent)-1)])
       parent.append(i)
   return children
 
 
 if __name__ == '__main__':
+  random.seed(7)
   n = int(sys.argv[1])
   print(n)
-  print(create_tree(n))
-  print("1")
-  print("1 1")
-
+  print(" ".join(map(lambda x: str(x),create_tree(n))))
+  test_cases = random.randint(1,10)
+  print(test_cases)
+  for i in range(0, test_cases):
+    print("%d %d" % (random.randint(1,n), random.randint(0,100000)))
 
