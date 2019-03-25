d2d1c26b18a2cc7a100233203e8ce56113c849fe
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2019-03-25 13:31:52 +0200

    challenge passed

---
+++ generator/hw1-task1/generator3.py
@@ -0,0 +1,30 @@
+import random
+import sys
+
+def create_tree(n):
+  parent = []
+  parent.append(1)
+  children = []
+  for i in range(1, n):
+      children.append(parent[random.randint(0,len(parent)-1)])
+      parent.append(i)
+  return children
+
+
+if __name__ == '__main__':
+  n = int(sys.argv[1])
+  
+  #setting seed if it was passed, otherwise use default
+  if len(sys.argv) == 2:
+    s = 7
+  else:
+    s = int(sys.argv[2])
+
+  random.seed(s)
+  print(n)
+  print(" ".join(map(lambda x: str(x),create_tree(n))))
+  test_cases = random.randint(1,10)
+  print(test_cases)
+  for i in range(0, test_cases):
+    print("%d %d" % (random.randint(1,n), random.randint(1,int(10))))
+
